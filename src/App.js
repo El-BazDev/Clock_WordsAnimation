ac2adffb7c8c1fcad80b9d071ebab6f2fe31c1b9
El_BazDev <124913839+El-BazDev@users.noreply.github.com>
2025-02-12 00:27:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import HeadNav from './Components/Navbar/HeadNav'
+import HeadNav from './Components/Navbar/HeadNav';
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [arr] = useState(['Hello world!',"Bonjour monde!",'مرحبا بالعالم!','ハロー・ワールド!','Здравствуй, мир!',''])
+  const [toggle, setToggle] = useState(true);
+  const [greetings] = useState([
+    'Hello world!',
+    'Bonjour monde!',
+    'مرحبا بالعالم!',
+    'ハロー・ワールド!',
+    'Здравствуй, мир!',
+    ''
+  ]);
 
   useEffect(() => {
-    arr[currentIndex] == arr.slice(-1) && setToggle(!toggle)
+    if (greetings[currentIndex] === greetings.slice(-1)[0]) {
+      setToggle(prev => !prev);
+    }
+    
     const intervalId = setInterval(() => {
-      if (currentIndex === arr.length - 1) {
+      if (currentIndex === greetings.length - 1) {
         clearInterval(intervalId);
       } else {
-        setCurrentIndex(currentIndex + 1);
+        setCurrentIndex(prev => prev + 1);
       }
     }, 1000);
+    
     return () => clearInterval(intervalId);
-  }, [currentIndex, arr]);
-
-  const [toggle,setToggle] = useState(true)
+  }, [currentIndex, greetings]);
 
   return (
-    <div className="App" >
-      {toggle && <h1 className="fade-in" style={{ animationDelay: `${currentIndex * 1000}ms` }}>{arr[currentIndex]}</h1> }
-      {toggle===false && <HeadNav/>}
+    <div className="App">
+      {toggle && (
+        <h1 
+          className="fade-in" 
+          style={{ animationDelay: `${currentIndex * 1000}ms` }}
+        >
+          {greetings[currentIndex]}
+        </h1>
+      )}
+      {!toggle && <HeadNav />}
     </div>
   );
 }
